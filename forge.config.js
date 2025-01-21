9f9dae0d9e53ee01e784a97a5e074fd221264546
mansanzhao <11508284+mansanzhao@user.noreply.gitee.com>
2025-01-21 20:55:41 +0800

    测试

--- forge.config.js
+++ forge.config.js
@@ -1,44 +1,77 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
+// module.exports = {
+//   packagerConfig: {
+//     asar: true,
+//   },
+//   rebuildConfig: {},
+//   makers: [
+//     {
+//       name: '@electron-forge/maker-squirrel',
+//       config: {},
+//     },
+//     {
+//       name: '@electron-forge/maker-zip',
+//       platforms: ['darwin'],
+//     },
+//     {
+//       name: '@electron-forge/maker-deb',
+//       config: {},
+//     },
+//     {
+//       name: '@electron-forge/maker-rpm',
+//       config: {},
+//     },
+//   ],
+//   plugins: [
+//     {
+//       name: '@electron-forge/plugin-auto-unpack-natives',
+//       config: {},
+//     },
+//     // Fuses are used to enable/disable various Electron functionality
+//     // at package time, before code signing the application
+//     new FusesPlugin({
+//       version: FuseVersion.V1,
+//       [FuseV1Options.RunAsNode]: false,
+//       [FuseV1Options.EnableCookieEncryption]: true,
+//       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+//       [FuseV1Options.EnableNodeCliInspectArguments]: false,
+//       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+//       [FuseV1Options.OnlyLoadAppFromAsar]: true,
+//     }),
+//   ],
+// };
+
 module.exports = {
   packagerConfig: {
-    asar: true,
+    icon: './assets/icon',
   },
-  rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name: 'yuanquWeb',
+      },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "win32"
+      ]
+    }
   ],
-  plugins: [
+  publishers: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'mansanzhao', // GitHub用户名
+          name: 'yunaquweb',      // 仓库名
+        },
+        prerelease: false,
+        draft: false,
+      },
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
   ],
 };
